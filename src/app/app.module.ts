1ee691d2ad56a1e2a0ae4eb0343cb2a0feb008e8
Trent <mr.peschke@gmail.com>
2019-02-15 21:50:07 -0700

    styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ViewComponent } from './view/view.component';
 import { AppRoutingModule } from './app-routing.module';
-import { BoardsContainerComponent } from './boards-container/boards-container.component';
 import { GoalsContainerComponent } from './goals-container/goals-container.component';
 
 @NgModule({
@@ -14,7 +13,6 @@ import { GoalsContainerComponent } from './goals-container/goals-container.compo
     AppComponent,
     HomeComponent,
     ViewComponent,
-    BoardsContainerComponent,
     GoalsContainerComponent
   ],
   imports: [
